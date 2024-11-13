bbb5094b14c390a1acabf1ffe3c4a42769b1e567
gooflee <gooflee@yahoo.com>
2024-11-13 06:45:21 -0500

    Added cucumber integration

--- source/pages/HomePage.cs
+++ source/pages/HomePage.cs
@@ -1,6 +1,7 @@
 
 
 using OpenQA.Selenium;
+using OpenQA.Selenium.DevTools.V128.Network;
 using OpenQA.Selenium.Interactions;
 
 
@@ -13,8 +14,18 @@ public class HomePage : BasePage {
     [FindsBy(How = How.XPath, Using ="//a[contains(text(),'About')]")]
     private IWebElement? _aboutMenu;
 
-    [FindsBy(How=How.XPath, Using = "//header/div[1]/nav[1]/div[2]/div[1]/div[3]/ul[1]/li[2]")]
+    [FindsBy(How=How.XPath, Using = "//a[contains(text(),'Features')]")]
     private IWebElement? _FeaturesMenu;
+
+    [FindsBy(How = How.XPath, Using = "//a[contains(text(),'Contacts')]")]
+    private IWebElement? _contactMenuItem;
+    
+    [FindsBy(How=How.Id, Using = "messenger-button")]
+    private IWebElement _messageButton;
+
+    [FindsBy(How=How.TagName, Using = "textarea")]
+    private IWebElement _messagerTextBox;
+
     public HomePage(IWebDriver driver) : base(driver){
         PageFactory.InitElements(driver, this);
     }
@@ -32,5 +43,19 @@ public class HomePage : BasePage {
     public void hoverOverFeaturesMenu(){
         Actions actions = new Actions(_driver);
         actions.MoveToElement(_FeaturesMenu).Build().Perform();
+    }
+
+    public void clickContactsMenuItem(){
+        this.hoverOverFeaturesMenu();
+        _contactMenuItem.Click();
+    }
+
+    public void clickMessengerButton(){
+        _messageButton.Click();
+    }
+
+    public void typeTextIntoMessagerTextfield(string message){
+        _driver.SwitchTo().Frame(1);
+        _messagerTextBox.SendKeys(message);
     }
 }
--- tests/HomePageTest.cs
+++ tests/HomePageTest.cs
@@ -1,6 +1,7 @@
 
 
 
+using AngleSharp.Text;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using WebDriverManager;
@@ -21,11 +22,30 @@ public class HomePageTest : BaseTest{
     }
 
     [Test]
-    public void clickSomethingWithHoverOver(){
+    public void hoverOverFeatures(){
         driver.Navigate().GoToUrl("https://freecrm.com/");
         Assert.IsTrue(driver.Title=="#1 Free CRM Software Power Up your Entire Business Free Forever");
         HomePage homePage = new HomePage(driver);
         homePage.hoverOverFeaturesMenu();
+        var a = 1;
+    }
+
+    [Test]
+    public void clickContactsMenu(){
+        driver.Navigate().GoToUrl("https://freecrm.com/");
+        Assert.IsTrue(driver.Title=="#1 Free CRM Software Power Up your Entire Business Free Forever");
+        HomePage homePage = new HomePage(driver);
+        homePage.clickContactsMenuItem();
+        Assert.That(driver.Title,Is.EqualTo("CRM Contacts for Business and CRM Cloud Software from Free CRM"));
+    }
+
+    [Test]
+    public void chatWithMessager(){
+        driver.Navigate().GoToUrl("https://freecrm.com/");
+        Assert.IsTrue(driver.Title=="#1 Free CRM Software Power Up your Entire Business Free Forever");
+        HomePage homePage = new HomePage(driver);
+        homePage.clickMessengerButton();
+        homePage.typeTextIntoMessagerTextfield("This may not work");
         var a = 1;
     }
     
